d4f41305768461bae04b7b00a6e3a4696014e286
haru <haruki1472@icloud.com>
2022-07-19 22:28:40 +0900

    vue first commit helloworld

---
+++ front/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import NewView from "../views/NewView";
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/new",
+    name: "new",
+    component: NewView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
---
+++ front/src/views/NewView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>New</h2>
+  <div><input type="text" /></div>
+  <div><textarea></textarea></div>
+  <div class="center">
+    <button>保存</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  margin-bottom: 10px;
+}
+input[type="text"] {
+  width: 100%;
+}
+textarea {
+  width: 100%;
+  height: 30em;
+}
+button {
+  width: 5em;
+  margin: 3px;
+}
+.center {
+  text-align: center;
+}
+</style>
