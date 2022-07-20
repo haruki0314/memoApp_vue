dd5b778cced2bd7631cefcd6e7dd6f2f03106bf0
haru <haruki1472@icloud.com>
2022-07-20 20:47:29 +0900

    Memoファイルとしてコンポーネント化

--- front/src/views/NewView.vue
+++ front/src/views/NewView.vue
@@ -1,28 +1,12 @@
 <template>
-  <h2>New</h2>
-  <div><input type="text" /></div>
-  <div><textarea></textarea></div>
-  <div class="center">
-    <button>保存</button>
-  </div>
+  <MemoForm />
 </template>
-
-<style scoped>
-div {
-  margin-bottom: 10px;
-}
-input[type="text"] {
-  width: 100%;
-}
-textarea {
-  width: 100%;
-  height: 30em;
-}
-button {
-  width: 5em;
-  margin: 3px;
-}
-.center {
-  text-align: center;
-}
-</style>
+<script>
+import MemoForm from "../components/MemoForm.vue";
+export default {
+  name: "NewView",
+  components: {
+    MemoForm,
+  },
+};
+</script>
